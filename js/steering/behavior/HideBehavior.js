f684d777364873a2de7544c6608eb4ec3bb0b2fc
Oguz <eroglu.oguz93@gmail.com>
2020-05-13 22:04:23 +0200

    HideBehavior vol2

--- js/steering/behavior/HideBehavior.js
+++ js/steering/behavior/HideBehavior.js
@@ -1,5 +1,6 @@
 import { ArriveBehavior } from "./ArriveBehavior";
 import { VectorPool } from "../../core/VectorPool";
+import { Vector3D } from "../../core/Vector3D";
 import { Steerable } from "../Steerable";
 
 var vectorPool = new VectorPool(10);
@@ -9,6 +10,8 @@ var HideBehavior = function(steerable, options){
     satisfactionRadius: options.arriveSatisfactionRadius,
     slowDownRadius: options.arriveSlowDownRadius
   });
+
+  this.hideDistance = options.hideDistance;
 }
 
 HideBehavior.prototype = Object.create(ArriveBehavior.prototype);
@@ -21,6 +24,19 @@ HideBehavior.prototype.compute = function(){
   if (!steerable.hideTargetEntity){
     return this.result;
   }
+}
+
+HideBehavior.prototype.getHidingPosition = function(hideableEntity){
+  var steerable = this.steerable;
+  var targetPosition = steerable.hideTargetEntity.position;
+
+  var hideableRadius = hideableEntity.box.getBoundingRadius();
+
+  var distanceAwayFromHideable = hideableRadius + this.hideDistance;
+
+  var hideableEntityPosition = hideableEntity.position;
+
+  return vectorPool.get().copy(hideableEntityPosition).sub(targetPosition).normalize().multiplyScalar(distanceAwayFromHideable).add(hideableEntityPosition);
 }
 
 Object.defineProperty(HideBehavior.prototype, 'constructor', { value: HideBehavior,  enumerable: false, writable: true });
--- test/steering/behavior/HideBehaviorTest.js
+++ test/steering/behavior/HideBehaviorTest.js
@@ -6,18 +6,53 @@ describe("HideBehavior", function(){
   it("should initialize", function(){
 
     var steerable = new Kompute.Steerable("steerable1", new Kompute.Vector3D(), new Kompute.Vector3D(10, 10, 10));
-    var hideBehavior = new Kompute.HideBehavior(steerable, { arriveSatisfactionRadius: 50, arriveSlowDownRadius: 100 });
+    var hideBehavior = new Kompute.HideBehavior(steerable, {
+      arriveSatisfactionRadius: 50,
+      arriveSlowDownRadius: 100,
+      hideDistance: 150
+    });
 
     expect(hideBehavior.result).to.eql(new Kompute.SteerResult());
     expect(hideBehavior.steerable).to.equal(steerable);
     expect(hideBehavior.satisfactionRadius).to.eql(50);
     expect(hideBehavior.slowDownRadius).to.eql(100);
+    expect(hideBehavior.hideDistance).to.eql(150);
   });
 
   it("should not request acceleration if steerable has no hide target entity", function(){
     var steerable = new Kompute.Steerable("steerable1", new Kompute.Vector3D(), new Kompute.Vector3D(10, 10, 10));
-    var hideBehavior = new Kompute.HideBehavior(steerable, { arriveSatisfactionRadius: 50, arriveSlowDownRadius: 100 });
+    var hideBehavior = new Kompute.HideBehavior(steerable, {
+      arriveSatisfactionRadius: 50,
+      arriveSlowDownRadius: 100,
+      hideDistance: 150
+    });
 
     expect(hideBehavior.compute().linear).to.eql(new Kompute.Vector3D());
+  });
+
+  it("should get hiding position", function(){
+    var steerable = new Kompute.Steerable("steerable1", new Kompute.Vector3D(), new Kompute.Vector3D(10, 10, 10));
+    var hideTarget = new Kompute.Steerable("steerable2", new Kompute.Vector3D(100, 200, 300), new Kompute.Vector3D(10, 10, 10));
+
+    var hideBehavior = new Kompute.HideBehavior(steerable, {
+      arriveSatisfactionRadius: 50,
+      arriveSlowDownRadius: 100,
+      hideDistance: 150
+    });
+
+    steerable.setHideTargetEntity(hideTarget);
+
+    var hideableEntity = new Kompute.Entity("hideable", new Kompute.Vector3D(-100, 200, -450), new Kompute.Vector3D(100, 100, 100));
+
+    var hidingPosition = hideBehavior.getHidingPosition(hideableEntity);
+
+    var testBox = new Kompute.Box(new Kompute.Vector3D(), new Kompute.Vector3D()).setFromTwoVectors(hideTarget.position, hidingPosition, 0.001);
+
+    expect(testBox.intersectsBox(hideableEntity.box)).to.eql(true);
+
+    var v1 = new Kompute.Vector3D().copy(hidingPosition).sub(hideTarget.position).normalize();
+    var v2 = new Kompute.Vector3D().copy(hidingPosition).sub(hideableEntity.position).normalize();
+
+    expect(v1.dot(v2)).to.eql(1);
   });
 });
